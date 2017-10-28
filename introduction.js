683a02b2d9ee3fa5d411b4dfb4ac86fc0d32f433
Argir Argirov <dj.giro@gmail.com>
2017-10-28 15:49:09 -0400

    classes and inheritance

--- introduction.js
+++ introduction.js
@@ -1,6 +1,87 @@
-var example = 'example string';
+// js practice
 
+var example, example2;
+
+const name = 'darcy'
+const person = {
+  name: 'ari',
+  changeHair: function () {
+    // this = person object
+    person.hair = 'blue'
+  },
+  driveCar: function (type) {
+    console.log(`${person.name} is driving a ${type}`)
+  },
+  age: 52,
+  licence: null
+}
+
+// usage of a constant
+person.name = 'ahmad' // fine
+person.changeHair()
+person.driveCar('BMW')
+// person = {} // throw and error
+
+// create a class
+class animal {
+  constructor (value) {
+    this._legs = value
+  }
+  run () {
+    console.log('animal is running')
+  }
+  get legs () {
+    return this._legs
+  }
+  set legs (value) {
+    this._legs = value
+  }
+}
+
+// use that class
+var cat = new animal(2)
+cat.legs = 4
+console.log(cat.legs)
+cat.run()
+
+// extend that class
+class dog extends animal {
+  constructor () {
+    super()
+  }
+}
+var lassy = new dog(6)
+console.log(lassy.legs)
+
+
+// usage of a function with private members
+function people (type) {
+  var privateValue = 'foo'
+  this.heritage = type
+  this.migrate = function () {
+    console.log(`move the ${this.heritage} people north`)
+  }
+}
+
+var canadians = new people('canadian')
+canadians.migrate()
+console.log(canadians.heritage) // canadian
+console.log(canadians.privateValue) // undefined
+
+example = 'example string'
 example = example.replace('string', 'writing')
 
+console.log(example2);
 
-console.log(example);
+for(var x = 0; x < 10; x++) {
+  example2 = x;
+  console.log(example2);
+}
+
+console.log(example2) // 10
+console.log(example)
+
+roundUp = 456.55
+h = Math.round(roundUp)
+
+console.log(h)
